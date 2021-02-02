739f7ed6f85248de9446c553a488dc2cfe12a530
ryo-murai <murai.ry@gmail.com>
2021-02-02 22:25:02 +0900

    generated by spring initializr

---
+++ src/main/java/com/example/studyoidc/StudyOidcApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyoidc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyOidcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyOidcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studyoidc/StudyOidcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyoidc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyOidcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
